65a90acb672e56e36d9abcc80c3433529ac1301d
jesus.sanchez@qacg.com <Jsanch37>
2019-02-19 15:49:06 -0600

    Modificaciones

--- server.js
+++ server.js
@@ -4,12 +4,32 @@ const path = require('path');
 
 const app = express();
 
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/contro-empleados'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/control-empleados/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
